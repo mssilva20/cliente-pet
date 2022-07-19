8057762a7e6860458b74b41e79ce4e4bc0acca2b
mssilva20 <mahteus10170@gmail.com>
2022-07-19 20:46:43 -0300

    Parte web testada

--- src/main/java/br/com/pet/Clientepetz/ClientePetzApplication.java
+++ src/main/java/br/com/pet/Clientepetz/ClientePetzApplication.java
@@ -2,10 +2,19 @@ package br.com.pet.Clientepetz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetzApplication {
-
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Pet - API Home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetzApplication.class, args);
 	}
