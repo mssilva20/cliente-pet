fda9c605cfe65e3477242aafcb8a407e8c54d232
mssilva20 <mahteus10170@gmail.com>
2022-08-05 11:03:38 -0300

    Teste

--- src/main/java/br/com/pet/Clientepetz/ClientePetzApplication.java
+++ src/main/java/br/com/pet/Clientepetz/ClientePetzApplication.java
@@ -2,10 +2,17 @@ package br.com.pet.Clientepetz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetzApplication {
 
+	public String getHOmeTest() {
+	return "Cliente Pet - Api home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetzApplication.class, args);
 	}
